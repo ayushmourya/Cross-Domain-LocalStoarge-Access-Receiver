2bdf70a5c757d6e8b73a35a1d66addd297a27cce
mirats-sde <101569551+mirats-sde@users.noreply.github.com>
2022-09-14 17:36:26 +0530

    this works

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+var createHost = require("cross-domain-storage/host");
+var createGuest = require("cross-domain-storage/guest");
+
+var storageHost = createHost([
+  {
+    origin: "https://mirats-2.netlify.app",
+    allowedMethods: ["get", "set", "remove"],
+  },
+  {
+    origin: "https://mirats-1.netlify.app",
+    allowedMethods: ["get"],
+  },
+]);
 
 function App() {
+  const [localValue, setLocalValue] = useState("");
+  const [crossDomainValue, setCrossDomainValue] = useState("");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p> Cross Doamin Storage</p>
+        <input
+          placeholder="Enter your value"
+          onChange={(e) => {
+            setLocalValue(e.target.value);
+          }}
+          value={localValue}
+        ></input>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            localStorage.setItem("localStorageKey", localValue);
+            setLocalValue("");
+          }}
         >
-          Learn React
-        </a>
+          {" "}
+          Save to Storage
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            var bazStorage = createGuest(
+              window.location.href === "https://mirats-1.netlify.app/"
+                ? "https://mirats-2.netlify.app"
+                : "https://mirats-1.netlify.app/"
+            );
+            bazStorage.get("localStorageKey", function (error, value) {
+              // value for the key of 'fizz' will be retrieved from localStorage on www.baz.com
+              if (error) {
+                console.log(error);
+              } else {
+                setCrossDomainValue(value);
+              }
+            });
+          }}
+        >
+          {" "}
+          Access Storage{" "}
+        </button>
+        <p>
+          Value Stored from current: {localStorage.getItem("localStorageKey")}
+        </p>
+        <p>Value Stored from cross current: {crossDomainValue}</p>
       </header>
     </div>
   );
